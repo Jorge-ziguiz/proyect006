eaee28c94172114ffa061d42d597bcb9dba8731e
n <n@n.n>
2025-07-15 21:35:43 +0200

    primer commit

---
+++ src/main/java/es/cic/curso25/proyecto006/Proyecto006Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyecto006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto006Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso25/proyecto006/Proyecto006ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyecto006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
